03804fe19db3e09905c143d29e941404b558c7ac
AlexMorgos <morgos.alexander@gmail.com>
2019-10-16 14:34:57 +0300

    feature/id-#5/task-from-a-video-tutorial

---
+++ src/vschoolProducts.js
@@ -0,0 +1,27 @@
+const products = [
+
+    {
+        id: "1",
+        name: "Macbook",
+        price: "1800",
+        description: "computer"
+    },
+    {
+        id: "2",
+        name: "iPhone",
+        price: "1100",
+        description: "phone"
+    },
+    {
+        id: "3",
+        name: "AirPods",
+        price: "200",
+        description: "headset"
+    },
+    {
+        id: "4",
+        name: "AppleWatch",
+        price: "500",
+        description: "smart watch"
+    }
+]
