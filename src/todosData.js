808164134192ed9c75835865e57a55cefd2cf2ca
AlexMorgos <morgos.alexander@gmail.com>
2019-10-16 15:16:50 +0300

    feature/id-#6/task-from-a-video-tutorial

---
+++ src/todosData.js
@@ -0,0 +1,19 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Buy some vodka",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Get drunk",
+        completed: true
+    },
+    {
+        id: 3,
+        text: "Stay alive",
+        completed: false
+    }
+]
+
+export default todosData;
